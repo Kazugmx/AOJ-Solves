a83b9dafcb77b40ba4bbbb4f481647ef57cd46fe
kazu <hikarukiyora@gmail.com>
2023-09-16 04:39:03 +0900

    wip-solve joi challenges

--- src/JOIPremlim_0708.java
+++ src/JOIPremlim_0708.java
@@ -1,5 +1,27 @@
-public class JOIPremlim_0708 {
-    public static void main(String... args){
+import java.util.Scanner;
+import java.util.stream.Stream;
 
+public class JOIPremlim_0708 {
+    static Scanner sc = new Scanner(System.in);
+    public static void main(String... args){
+        sc.nextLine();
+        int[] al = Stream.of(sc.nextLine().split(" ")).
+                mapToInt(Integer::parseInt).toArray();
+        int[] bl = Stream.of(sc.nextLine().split(" ")).
+                mapToInt(Integer::parseInt).toArray();
+        int[] cl = Stream.of(sc.nextLine().split(" ")).
+                mapToInt(Integer::parseInt).toArray();
+        int[] dl = Stream.of(sc.nextLine().split(" ")).
+                mapToInt(Integer::parseInt).toArray();
+        System.out.println(loadMin(al,bl,cl,dl));
+    }
+    //腹減った 飯食わせろ　2023/09/16 4:33
+    //今金なんかないです
+    public static int loadMin(int[] aq ,int[] bq ,int[] cq ,int[] dq){
+        int mindiff = Integer.MAX_VALUE;
+
+        int[] co = new int[4];
+
+        return mindiff;
     }
 }
